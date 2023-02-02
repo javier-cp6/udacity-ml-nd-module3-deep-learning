7f88d9e09755d6c7e6b5a863ce931339507dce23
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2023-02-02 17:04:30 -0500

    initial commit

---
+++ l2_deeplearning_intro/1_build_a_neural_network/main.py
@@ -0,0 +1,17 @@
+from torch import nn
+
+def create_model():
+    #TODO: Build and return a feed-forward network
+    input_size = 784
+    output_size = 10
+    model = nn.Sequential(nn.Linear(input_size, 128),
+                          nn.ReLU(),
+                          nn.Linear(128, 64),
+                          nn.ReLU(),
+                          nn.Linear(64, 32),
+                          nn.ReLU(),
+                          nn.Linear(32, output_size),
+                          nn.LogSoftmax(dim=1))
+    return model
+    
+    
