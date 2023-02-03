578878910d7536ec244812d02d8b2be2f62a22dc
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2023-02-02 23:47:58 -0500

    l4 ex1 script-mode completed

--- l4_sagemaker_deployment/2_deployment_cifar10/script_mode/scripts/pytorch_cifar.py
+++ l4_sagemaker_deployment/2_deployment_cifar10/script_mode/scripts/pytorch_cifar.py
@@ -8,9 +8,29 @@ from torchvision import datasets, transforms
 class Net(nn.Module):
     def __init__(self):
         #TODO: Complete this function
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(64*14*14, 128)
+        self.fc2 = nn.Linear(128, 10)
         
     def forward(self, x):
         #TODO: Complete the forward function
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 def train(model, train_loader, optimizer, epoch):
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -52,24 +72,60 @@ def test(model, test_loader):
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
+    parser = argparse.ArgumentParser(description="PyTorch CIFAR Example")
     # TODO: Add your arguments here
-    args = parser.parse_args()
-
-    transform = # TODO: Create your transform
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=14,
+        metavar="N",
+        help="number of epochs to train (default: 14)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)"
+    )
     
-    # TODO: Add the CIFAR10 dataset and create your data loaders
-    dataset = 
-    train_loader =
-    test_loader = 
+    args = parser.parse_args()
+    
+    train_kwargs = {"batch_size": args.batch_size}
+    test_kwargs = {"batch_size": args.test_batch_size}
+
+    # TODO: Create your transform
+    transform = transforms.Compose(
+        [transforms.ToTensor()]
+    )
+    
+    # TODO: Add the CIFAR10 dataset and create your data loaders 
+    dataset1 = datasets.CIFAR10("../data", train=True, download=True, transform=transform)
+    dataset2 = datasets.CIFAR10("../data", train=False, transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net()
 
-    optimizer = # TODO: Add your optimizer
+    # TODO: Add your optimizer
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     for epoch in range(1, args.epochs + 1):
         train(model, train_loader, optimizer, epoch)
         test(model, test_loader)
+    
+    #save
+    torch.save(model.state_dict(), "cifar_cnn.pt")
 
 
 if __name__ == "__main__":
